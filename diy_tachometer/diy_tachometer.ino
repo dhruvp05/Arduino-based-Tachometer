a31dbd2863e8f7862e4d29922297e8b8710638c6
dhruv <dhruvpatel1234.dp39@gmail.com>
2024-03-12 18:12:19 +0530

    1) IR sensor working properly with the DC motor i.e. showing 0 and 1. 2) count of the timer is showing 0. 3) Pro Micro not working.

--- diy_tachometer/diy_tachometer.ino
+++ diy_tachometer/diy_tachometer.ino
@@ -1,15 +1,16 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+// LiquidCrystal_I2C lcd(0x27,16,2);
 
 volatile unsigned int count;
 unsigned int rpm;
 bool too_slow = false;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(2,INPUT);
-  lcd.init();
-  lcd.backlight();
+  // lcd.init();
+  // lcd.backlight();
   attachInterrupt(digitalPinToInterrupt(2),fallingISR,FALLING);
   TCCR1A = 0;
   TCCR1B = 0;
@@ -19,15 +20,22 @@ void setup() {
 }
 
 void loop() {
-  rpm = 60*(62500/count);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(rpm);
+  Serial.println(count);
+  if(too_slow==false){
+    rpm = 60*(62500/count);
+    Serial.println(rpm);
+    // lcd.clear();
+    // lcd.setCursor(0,0);
+    // lcd.print(rpm);
+    delay(1500);
+  }
 
   if(too_slow==true){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("TOO SLOW!");
+    Serial.println("TOO SLOW!");
+    // lcd.clear();
+    // lcd.setCursor(0,0);
+    // lcd.print("TOO SLOW!");
+    delay(1500);
   }
 }
 
